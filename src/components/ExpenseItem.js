47b6db6f6ffdcc389755bc51d8b9874e8d353742
richmondtanch <richmondtanch@gmail.comgit config --global --listgit config --global user.email richmondtanch@gmail.com>
2022-10-26 14:21:07 +0800

    create new expense component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+
+function ExpenseItem(props) {
+    //const expenseDate = new Date(2022,10,5);
+    //const ExpenseTitle = 'Car Insurance';
+    //const expenseAmount = '297.67';
+
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
